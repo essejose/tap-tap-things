46ce66aa0cf1f54a8c02644a8e390f6206508bbe
essejose <ze_pk2@hotmail.com>
2017-01-15 19:06:51 -0200

    working  pause game

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -12,4 +12,17 @@ public class GameController : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
+    public void PauseGame()
+    { 
+         Time.timeScale = 0f; 
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+
+    }
+
 }
