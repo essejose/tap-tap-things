461f12db16e9e62a6129fa26987cdf2b9b57f6b1
essejose <ze_pk2@hotmail.com>
2017-01-16 23:05:06 -0200

    Pause game done

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -3,24 +3,49 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static GameController instance;
+
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
 
+    }
+    void Awake()
+    {
+        MakeSingleton();
+      
+    }
 
+    void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     public void PauseGame()
     { 
-         Time.timeScale = 0f; 
+         Time.timeScale = 0f;
+        pausePanel.SetActive(true);
     }
 
     public void ResumeGame()
     {
+        pausePanel.SetActive(false);
         Time.timeScale = 1f;
 
     }
